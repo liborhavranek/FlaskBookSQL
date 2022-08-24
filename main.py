9ee8bc6b737c7540db5ce083f4d24b34bcf21d01
liborhavranek <liborhavranek91@gmail.com>
2022-08-24 19:46:35 +0200

    sql with form

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
 
-# all_books = [  {
-#         "title": "Harry Potter",
-#         "author": "J. K. Rowling",
-#         "rating": 9,
-#     }]
+# vztvoreni databaze
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
 
-all_books = []
+
+# vytvoreni tabulky
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+db.create_all()
+
+
+
 
 @app.route('/')
 def home():
-    return render_template('index.html', books=all_books)
+    all_books = db.session.query(Book).all()
+    return render_template('index.html', book_stack=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
-
-        # NOTE: You can use the redirect method from flask to redirect to another route
-        # e.g. in this case to the home page after the form has been submitted.
+        b_name = request.form["title"]
+        b_author = request.form["author"]
+        b_rating = request.form["rating"]
+        new_book = Book(title=b_name, author=b_author, rating=b_rating)
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
-
-    return render_template("add.html")
-
+    return render_template('add.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
